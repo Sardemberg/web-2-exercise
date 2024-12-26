7b65f9882c2612bb728c21b7192c29f9d9d69050
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-12-26 17:51:50 -0300

    feat: criado base do endpoint valida login

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const { json } = require('express');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const app = express();
 app.use(json());  // Note que json precisa ser chamado como função
 
 //rota para criar um acesso
 app.post('/acesso', (req, res) => {
+    const { usuario, senha } = req.body;
+    // Verifica se o usuário existe no banco de dados
+
+    // Se o usuário existe, verifica se a senha está correta
+
+    // Se a senha está correta, retorna um true
+    // Se a senha está incorreta, retorna um false
+
+    // Caso usuário não exista, cria um novo usuário
 
 });
 
 //rota para logar
-app.post('/login', (req, res) => {
-
+app.post('/validaLogin', (req, res) => {
+    try {
+        const { usuario, senha } = req.body;
+        // usando arquivo env por enquanto que não temos conexão com banco de dados
+        if (usuario === process.env.USUARIO && senha === process.env.SENHA) {
+            let novoToken = jwt.sign({ usuario }, process.env.APP_KEY, { expiresIn: 9000 });
+            res.json({ logado: true, token: novoToken });
+        } else {
+            res.json({ logado: false, mensagem: 'Usuário ou senha errados.' });
+        }
+    } catch (error) {
+        res.json({ logado: false, mensagem: 'Erro durante o login.' });
+    }
 });
 
 //rota para Laboratório novo
