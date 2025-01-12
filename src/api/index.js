f380a3d8ab8db40d4a34bda07378c4664e30c960
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-01-12 17:48:26 -0300

    refactor: modularizing code

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,20 @@
-const { config } = require('dotenv')
-const { connectDB } = require('../config/mongodb')
+const { config } = require('dotenv');
+const { connectDB } = require('../config/mongodb');
 const { json } = require('express');
 const express = require('express');
-<<<<<<< HEAD:index.js
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-=======
-const { dayMiddleware } = require("./middlewares/dayMiddleware")
-const { labReportController } = require("./controllers/labReportController")
+const { dayMiddleware } = require("./middlewares/dayMiddleware");
+const { labReportController } = require("./controllers/labReportController");
+const { loginController } = require("./controllers/loginController");
 
-config()
-connectDB()
->>>>>>> main:src/api/index.js
+config();
+connectDB();
 const app = express();
 app.use(json());
 app.use(dayMiddleware);
 
 //rota para criar um acesso
 app.post('/acesso', (req, res) => {
-    const { usuario, senha } = req.body;
+    const { login, password } = req.body;
     // Verifica se o usuário existe no banco de dados
 
     // Se o usuário existe, verifica se a senha está correta
@@ -31,20 +27,7 @@ app.post('/acesso', (req, res) => {
 });
 
 //rota para logar
-app.post('/validaLogin', (req, res) => {
-    try {
-        const { usuario, senha } = req.body;
-        // usando arquivo env por enquanto que não temos conexão com banco de dados
-        if (usuario === process.env.USUARIO && senha === process.env.SENHA) {
-            let novoToken = jwt.sign({ usuario }, process.env.APP_KEY, { expiresIn: 9000 });
-            res.json({ logado: true, token: novoToken });
-        } else {
-            res.json({ logado: false, mensagem: 'Usuário ou senha errados.' });
-        }
-    } catch (error) {
-        res.json({ logado: false, mensagem: 'Erro durante o login.' });
-    }
-});
+app.post('/validaLogin', loginController);
 
 //rota para Laboratório novo
 app.post('/laboratorio', (req, res) => {
@@ -53,7 +36,6 @@ app.post('/laboratorio', (req, res) => {
 
 //rota para relatório de laboratórios
 app.get('/laboratorio/relatorio', labReportController);
-
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
